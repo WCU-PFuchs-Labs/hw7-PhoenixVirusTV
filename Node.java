84333dbbde5201a0321b5c5b7acb2ac5fe9f1a0e
Michael Williams <106316893+PhoenixVirusTV@users.noreply.github.com>
2025-12-01 22:30:29 -0500

    Node.java

--- Node.java
+++ Node.java
@@ -5,7 +5,9 @@ public class Node implements Cloneable {
     Op op;
 
     public Node(Binop op, Node left, Node right) { 
-        this.op = op; this.left = left; this.right = right; 
+        this.op = op; 
+        this.left = left; 
+        this.right = right; 
     }
 
     public Node(Binop op) { this(op, null, null); }
@@ -20,23 +22,23 @@ public class Node implements Cloneable {
     public void addRandomKids(NodeFactory nf, int maxDepth, Random rand) {
         if(maxDepth <= 0) return;
 
-        if(left == null) left = (rand.nextBoolean() ? nf.getOperator(rand) : nf.getTerminal(rand));
-        if(left.op instanceof Binop || left.op instanceof Unop) left.addRandomKids(nf, maxDepth - 1, rand);
-
         if(op instanceof Binop) {
-            if(right == null) right = (rand.nextBoolean() ? nf.getOperator(rand) : nf.getTerminal(rand));
-            if(right.op instanceof Binop || right.op instanceof Unop) right.addRandomKids(nf, maxDepth - 1, rand);
+            if(left == null) left = rand.nextBoolean() ? nf.getOperator(rand) : nf.getTerminal(rand);
+            if(right == null) right = rand.nextBoolean() ? nf.getOperator(rand) : nf.getTerminal(rand);
+            
+            left.addRandomKids(nf, maxDepth - 1, rand);
+            right.addRandomKids(nf, maxDepth - 1, rand);
         }
     }
 
     @Override
     public Object clone() {
         Node o = null;
-        try { o = (Node)super.clone(); } 
+        try { o = (Node) super.clone(); } 
         catch(CloneNotSupportedException e) { System.out.println("Node can't clone."); }
-        if(o.left != null) o.left = (Node)left.clone();
-        if(o.right != null) o.right = (Node)right.clone();
-        if(op != null) o.op = (Op)op.clone();
+        if(o.left != null) o.left = (Node) left.clone();
+        if(o.right != null) o.right = (Node) right.clone();
+        if(op != null) o.op = (Op) op.clone();
         return o;
     }
 
@@ -47,28 +49,28 @@ public class Node implements Cloneable {
         return "";
     }
 
-    /** Step 1 & 4: traversal for collector */
+    // Traversal for collector
     public void traverse(Collector c) {
         c.collect(this);
         if(left != null) left.traverse(c);
         if(right != null) right.traverse(c);
     }
 
-    /** Step 4: swap left child with trunk's left */
+    // Swap left child with another node's left
     public void swapLeft(Node trunk) {
         Node temp = this.left;
         this.left = trunk.left;
         trunk.left = temp;
     }
 
-    /** Step 4: swap right child with trunk's right */
+    // Swap right child with another node's right
     public void swapRight(Node trunk) {
         Node temp = this.right;
         this.right = trunk.right;
         trunk.right = temp;
     }
 
-    /** Step 4: check if leaf */
+    // Leaf check
     public boolean isLeaf() {
         return op instanceof Unop;
     }
